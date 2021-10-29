fbd2e244ee27cf0f80ee8637cc91b9ad1eb0ebbe
ThunderMan2300 <jaimeaaguilar@gmail.com>
2021-10-29 00:44:04 -0500

    Auction: Project Re-Start

--- src/main/java/com/System/Auction/Repositories/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.System.Auction.Repositories;
-
-import com.System.Auction.Entities.Member;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends CrudRepository<Member, Long> {
-}
