2aa3c8943fec7b45116e9a1a04250b6f438fc759
ThunderMan2300 <jaimeaaguilar@gmail.com>
2021-10-29 10:05:57 -0500

    Auction: Updated SQLs Queries

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,41 @@
 INSERT INTO MEMBER (MID, Name, HomeAddress, Email, Phone, Password) VALUES
-        (1, 'JAIME', 'FARMERS BRANCH', 'JAIME@GMAIL.COM', '1234567890', 'PASSWORD');
+        (1, 'JAIME', 'FARMERS BRANCH', 'JAIME@GMAIL.COM', '1234567890', 'PASSWORD'),
+        (2, 'ANNABELLE', 'RICHARDSON', 'NOTFAKE@GMAIL.COM', '5555555555', 'ABC'),
+        (3, 'JOSH', 'DALLAS', 'JOSHISCOOL@GMAIL.COM', '5551244483', '123'),
+        (4, 'AKSHAYA', 'RICHARDSON', 'MYEMAIL@GMAIL.COM', '9876453120', 'QWER'),
+        (5, 'ANISH', 'GARLAND', 'BUSINESS@GMAIL.COM', '123654987', '0000');
+
+INSERT INTO BUYER (MID, ShippingAddress) VALUES
+        (1, 'MY ADDRESS 1'),
+        (3, 'MY ADDRESS 2'),
+        (4, 'MY ADDRESS 3');
+
+INSERT INTO SELLER (MID, BankNum, RoutingNumber) VALUES
+        (2, '123456789102', '000000000'),
+        (5, '111111111111', '012345678');
+
+INSERT INTO SUPERCATEGORY (SupCategoryID, Name, Description) VALUES
+        (1, 'CARS', 'BROOM BROOM'),
+        (2, 'COMPUTERS', 'BEEP BOOP'),
+        (3, 'OUTDOORS', 'GOING OUT');
+
+INSERT INTO CATEGORY (SupCategoryID, CategoryID, CategoryName, Description) VALUES
+        (1, 1, 'MUSTANG', 'FORD SPORTS CAR'),
+        (2, 2, 'DELL', 'PARTNER WITH ALIENWARE'),
+        (2, 3, 'HP', 'HIGH PRINTER QUALITY');
+
+INSERT INTO ITEM (ItemID, SellerID, Title, Description, StartingPrice, EndTime, StartTime, BidIncrement, CategoryID, ImageURL) VALUES
+        (1, 2, 'NEW FORD MUSTANG', '2021 FORD MUSTANG CONVERTIBLE CAR', 250000, '2021-10-30', '2021-10-29', 500, 1, ''),
+        (2, 2, 'USED MOUSE', 'WORKING BLUETOOTH MOUSE', 5, '2021-10-28', '2021-10-21', 5, 3, '');
+
+INSERT INTO BID (BidID, BuyerID, ItemID, Price, Timestamp) VALUES
+        (1, 1, 1, 250000, '2021-10-29'),
+        (2, 3, 1, 250500, '2021-10-29'),
+        (3, 1, 1, 251000, '2021-10-29'),
+        (4, 3, 2, 5, '2021-10-27'),
+        (5, 4, 2, 10, '2021-10-28');
+
+INSERT INTO TRANSACTION (TransactionID, BuyerID, SellerID, ItemID, TransactionTime,
+ WinningBid, BuyerRating, SellerRating, BuyerFeedback, SellerFeedback) VALUES
+        (1, 1, 2, 1, '2021-10-30', 3, 5, 5, 'GREAT CAR', 'NICE LOOKING'),
+        (2, 4, 2, 2, '2021-10-28', 5, 3, 4, 'MOUSE IS OLD', 'SOWED UP LATE TO MEETING');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,13 @@
+DROP TABLE IF EXISTS TYPE;
+DROP TABLE IF EXISTS CATEGORY;
+DROP TABLE IF EXISTS SUPERCATEGORY;
+DROP TABLE IF EXISTS BIDS;
+DROP TABLE IF EXISTS TRANSACTION;
+DROP TABLE IF EXISTS ITEM;
+DROP TABLE IF EXISTS SELLER;
+DROP TABLE IF EXISTS BUYER;
 DROP TABLE IF EXISTS MEMBER;
+
 
 CREATE TABLE MEMBER (
 	MID 			long 			NOT NULL,
@@ -8,4 +17,80 @@ CREATE TABLE MEMBER (
 	Phone			varchar(10)		NOT NULL UNIQUE,
 	Password		varchar(255)	NOT NULL,
 	PRIMARY KEY (MID)
+);
+
+CREATE TABLE BUYER (
+	MID				long			NOT NULL,
+	ShippingAddress	varchar(255)	NOT NULL,
+	PRIMARY KEY (MID),
+	FOREIGN KEY (MID) REFERENCES MEMBER(MID)
+);
+
+CREATE TABLE SELLER (
+	MID				long			NOT NULL,
+	BankNum			varchar(12)		NOT NULL UNIQUE,
+	RoutingNumber	varchar(9)		NOT NULL,
+	PRIMARY KEY (MID),
+	FOREIGN KEY (MID) REFERENCES MEMBER(MID)
+);
+
+CREATE TABLE SUPERCATEGORY (
+	SupCategoryID	long			NOT NULL,
+	Name			varchar(255)	NOT NULL,
+	Description		varchar(255)			,
+	PRIMARY KEY (SupCategoryID)
+);
+
+CREATE TABLE CATEGORY (
+	SupCategoryID	long			NOT NULL,
+	CategoryID		long			NOT NULL,
+	CategoryName	varchar(255)	NOT NULL,
+	Description		varchar(255)			,
+	PRIMARY KEY (CategoryID),
+	FOREIGN KEY (SupCategoryID) REFERENCES SUPERCATEGORY(SupCategoryID)
+);
+
+CREATE TABLE ITEM (
+	ItemID			long			NOT NULL,
+	SellerID		long			NOT NULL,
+	Title			varchar(255)	NOT NULL,
+	Description		varchar(255)			,
+	StartingPrice	float			NOT NULL,
+	EndTime			date			NOT NULL,
+	StartTime		date			NOT NULL,
+	BidIncrement	float			NOT NULL,
+	CategoryID		float			NOT NULL,
+	ImageURL		varchar(255)	NOT NULL,
+	PRIMARY KEY (ItemID),
+	FOREIGN KEY (SellerID) REFERENCES SELLER(MID),
+	FOREIGN KEY (CategoryID) REFERENCES CATEGORY(CategoryID)
+);
+
+CREATE TABLE BID (
+	BidID			long			NOT NULL,
+	BuyerID			long			NOT NULL,
+	ItemID			long			NOT NULL,
+	Price			long			NOT NULL,
+	Timestamp		date			NOT NULL, --Check for timestamp variable type
+	PRIMARY KEY (BidID),
+	FOREIGN KEY (BuyerID) REFERENCES BUYER(MID),
+	FOREIGN KEY (ItemID) REFERENCES ITEM(ItemID)
+);
+
+CREATE TABLE TRANSACTION (
+	TransactionID	long			NOT NULL,
+	BuyerID			long			NOT NULL,
+	SellerID		long			NOT NULL,
+	ItemID			long			NOT NULL,
+	TransactionTime	date			NOT NULL,
+	WinningBid		long			NOT NULL,
+	BuyerRating		long					,
+	SellerRating	long					,
+	BuyerFeedback	varchar(255)			,
+	SellerFeedback	varchar(255)			,
+	PRIMARY KEY (TransactionID),
+	FOREIGN KEY (BuyerID) REFERENCES BUYER(MID),
+	FOREIGN KEY (SellerID) REFERENCES SELLER(MID),
+	FOREIGN KEY (ItemID) REFERENCES ITEM(ItemID),
+	FOREIGN KEY (WinningBid) REFERENCES BID(BidID)
 );
