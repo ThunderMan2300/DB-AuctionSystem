4d8c28060482231afb8abab0f864527af547be5d
ThunderMan2300 <jaimeaaguilar@gmail.com>
2021-11-21 18:52:30 -0600

    Auction: Main Controller and repository

--- src/main/java/com/System/Auction/Repositories/ItemRepository.java
+++ src/main/java/com/System/Auction/Repositories/ItemRepository.java
@@ -3,5 +3,8 @@ package com.System.Auction.Repositories;
 import com.System.Auction.Entities.Item;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.*;
+
 public interface ItemRepository extends CrudRepository<Item, Long> {
+    List<Item> findByEndTimeGreaterThan(Date date);
 }
